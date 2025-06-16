46596c4787959255a665758c67f8a869b2dd840e
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-06-16 18:24:26 +0530

    reverse array

--- Array/reverseArray.js
+++ Array/reverseArray.js
@@ -9,15 +9,42 @@
 // console.log(temp); // [50, 40, 30, 20, 10]
 
 
-let arr = [10, 20, 30, 40, 50];
+// let arr = [10, 20, 30, 40, 50];
 
-let i = 0, j = arr.length-1; // j is the last index of the array
+// let i = 0, j = arr.length-1; // j is the last index of the array
 
-while(i != j){
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+// while(i != j){
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+//     j--;
+// }
+// console.log(arr);
+
+
+// let arr = [1,2,3,4,5,6,7]
+
+// let i = 0, j = arr.length-1;
+
+// while(i != j){
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+//     j--;
+// }
+//  console.log(arr);
+
+
+let arr = [ 1,2,3,4,5,6,78]
+let temp = new Array(arr.length);
+
+let i = 0;
+
+for(let j=arr.length-1;j>=0;j--){
+    temp[i] = arr[j];
     i++;
-    j--;
 }
-console.log(arr);
+
+console.log(temp);
