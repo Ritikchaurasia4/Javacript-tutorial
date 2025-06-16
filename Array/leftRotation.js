48b8651a1c6192d1c83e05ac3f8852e5071f0566
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-06-16 18:22:06 +0530

    left rotation

--- Array/leftRotation.js
+++ Array/leftRotation.js
@@ -1,4 +1,4 @@
-// ................ Left Rotation of an array by 1 ..................
+// // ................ Left Rotation of an array by 1 ..................
 
 // let arr = [1, 2, 3, 4, 5];
 
@@ -25,4 +25,24 @@ for(let i=arr.length-1;i>0;i--){
 
 arr[0] = copy; // copy the first element of the array
 console.log(arr); 
+
+
+// -------------------------------left rotation of an array by k-----------------------------
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// // let k = Number(prompt("Enter k value ..."))
+// let k = 2;
+
+// for(let j=0;j<k;j++){
+//     let copy = arr[0];
+//     for(let i=0;i<arr.length-1;i++){
+//         arr[i] = arr[i+1]
+//     }
+    
+//     arr[arr.length-1] = copy;
+// }
+
+// console.log(arr);
 
